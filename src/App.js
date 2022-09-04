0178f070efd8cb7f508ead7d03cf1b6fb786a542
Fred Postles <fredpostles@freds-mbp.home>
2022-09-04 15:25:36 +0100

    AMade counter into component and added a second

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React, { Component } from "react";
+import Counter from "./components/Counter";
 import "./App.css";
 
 class App extends Component {
   state = { count: 0 };
 
-  onAdd = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  onRemove = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
-
   render() {
     return (
       <>
-        <div className="container">
+        {/* <div className="container">
           <h1>My Counter</h1>
           <p>{this.state.count}</p>
           <div>
             <button onClick={this.onAdd}>Add</button>
             <button onClick={this.onRemove}>Remove</button>
           </div>
+        </div> */}
+        <div className="container counter_1">
+          <h1>My Counter</h1>
+          <Counter onClick={(this.onAdd, this.onRemove)} />
+        </div>
+        <div className="container counter_2">
+          <h1>My Second Counter</h1>
+          <Counter onClick={(this.onAdd, this.onRemove)} />
         </div>
       </>
     );
